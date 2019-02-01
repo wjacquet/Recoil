5d5ecdd0d104c5c891f4adb0891dc032289820e5
Nicholas Fordyce <FordyceNick@gmail.com>
2019-02-01 14:08:10 -0500

    Player can aim and fire shots

---
+++ Recoil/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rigidBody = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+
+        GameObject cursor = GameObject.Find("obj_cursor");
+        Vector2 direction = cursor.transform.position - transform.position;
+        rigidBody.velocity = direction.normalized;
+    }
+}
---
+++ Recoil/Assets/Scripts/FireWeapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWeapon : MonoBehaviour
+{
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, transform.position, transform.rotation);
+    }
+}
