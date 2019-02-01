1423c2850646ebd10195d47b310a4ac2ffbffe83
Nicholas Fordyce <FordyceNick@gmail.com>
2019-02-01 15:34:50 -0500

    fix merge conflicts

---
+++ Recoil/Assets/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public void PlayeGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void EndGame() {
+        Debug.Log("Exiting Game!");
+        Application.Quit();
+    }
+}
