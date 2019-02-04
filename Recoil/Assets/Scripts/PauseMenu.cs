0b674d5bd0c2f9487cb785a80eaca9aa8b8c75f0
Kyle Brown <brow1144@purdue.edu>
2019-02-04 12:38:45 -0500

    Added P to bring up Pause Menu

--- Recoil/Assets/Scripts/PauseMenu.cs
+++ Recoil/Assets/Scripts/PauseMenu.cs
@@ -12,7 +12,7 @@ public class PauseMenu : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)) {
             if (gameIsPaused) {
                 Resume();
             } else {
