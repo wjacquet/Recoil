6924fb990c23b7413c303314643b086556fbb2c7
Walter Jacquette <wjacquet@purdue.edu>
2019-02-07 22:33:13 -0500

    You Die Now. Also bullets have no weight

--- Recoil/Assets/Scripts/Enemies/EnemyProjectile.cs
+++ Recoil/Assets/Scripts/Enemies/EnemyProjectile.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class EnemyProjectile : MonoBehaviour
 {
     // Start is called before the first frame update
+    PlayerHealth playerHP;
     void Start()
     {
         Rigidbody2D rigidBody = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
 
         // Find player and find the direction vector
         GameObject player = GameObject.Find("obj_player");
+        playerHP = player.GetComponent<PlayerHealth>();
         Vector2 direction = player.transform.position - transform.position;
         direction.y = 0;
         direction.Normalize();
@@ -20,6 +22,11 @@ public class EnemyProjectile : MonoBehaviour
     // Once the projectile hits a wall
     void OnCollisionEnter2D(Collision2D collision) 
     {
+        print(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Player") {
+            playerHP.TakeDamage();
+        }
         Destroy(gameObject);
     }
+
 }
---
+++ Recoil/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int initialHP = 3;
+    public int currHP = 3;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage() 
+    {
+        currHP--;
+        if (currHP <= 0) {
+            // Dead
+            Die();
+        }
+    }
+
+    public void UpgradeHP() 
+    {
+        initialHP++;
+        currHP++;
+    }
+
+    void Die() 
+    {
+        Destroy(gameObject);
+    }
+}
