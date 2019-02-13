bb402f0a74adba371ee75a2cd186a21f33777e63
Walter Jacquette <wjacquet@purdue.edu>
2019-02-13 13:31:44 -0500

    Mine start

--- Recoil/Assets/Scripts/Enemies/MineProjectile.cs
+++ Recoil/Assets/Scripts/Enemies/MineProjectile.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class MineProjectile : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int mineHP = 1;
 
     // Update is called once per frame
     void Update()
