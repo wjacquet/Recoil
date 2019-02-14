4259d364dc69a0488ed6aa15fe26ae27c879be0d
Nicholas Fordyce <FordyceNick@gmail.com>
2019-02-13 22:24:55 -0500

    Basic camera movement

---
+++ Recoil/Assets/Scripts/Player/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called once per frame after Update
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+        print(transform.position);
+    }
+}
