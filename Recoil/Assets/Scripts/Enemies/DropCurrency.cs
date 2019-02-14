82baeaa0cb55c4947d79ee50ca3d67c7d9bf645a
Walter Jacquette <wjacquet@purdue.edu>
2019-02-13 21:20:34 -0500

    Currency Creation

---
+++ Recoil/Assets/Scripts/Enemies/DropCurrency.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropCurrency : MonoBehaviour
+{
+    public GameObject coin;
+    public int amount;
+
+    // Update is called once per frame
+    public void DropCoins()
+    {
+        
+    }
+}
