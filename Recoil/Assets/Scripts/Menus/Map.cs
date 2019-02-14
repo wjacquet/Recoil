4c615ddd28918049cf89741f1d1f0d0ad630c5e9
Kyle Brown <brow1144@purdue.edu>
2019-02-14 12:11:05 -0500

    Map overlay now showing with 'M'

---
+++ Recoil/Assets/Scripts/Menus/Map.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public static bool gameIsPaused = false;
+
+    public GameObject mapUI;
+
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.M)) {
+            if (gameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        mapUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause() {
+        mapUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
