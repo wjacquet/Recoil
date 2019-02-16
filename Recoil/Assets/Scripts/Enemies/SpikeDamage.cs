4187d613e694e5a98ff21dcf1866287d0e8eb20c
Nicholas Fordyce <FordyceNick@gmail.com>
2019-02-15 20:58:10 -0500

    wip spike

---
+++ Recoil/Assets/Scripts/Enemies/SpikeDamage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeDamage : MonoBehaviour
+{
+    public GameObject player;
+    PlayerHealth playerHP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find player
+        playerHP = player.GetComponent<PlayerHealth>();
+    }
+
+    // Hurt the player on contact
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
+        print(collision);
+        if (collision.gameObject.tag == "Player") {
+            playerHP.TakeDamage();
+        }
+    }
+}
