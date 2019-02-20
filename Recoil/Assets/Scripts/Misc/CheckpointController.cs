11c2f6a13d30d19b4723d03da22d80a24cdf2e31
Walter Jacquette <wjacquet@purdue.edu>
2019-02-19 22:59:20 -0500

    Saving/loading with hard coded data fully functional - update player object

--- Recoil/Assets/Scripts/Misc/CheckpointController.cs
+++ Recoil/Assets/Scripts/Misc/CheckpointController.cs
@@ -7,8 +7,8 @@ public class CheckpointController : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
             print("Saving game");
-            //DataControl.Save();
-            DataControl.Load();
+            DataControl.Save();
+            //DataControl.Load();
         }
     }
 }
--- Recoil/Assets/Scripts/Player/PlayerInit.cs
+++ Recoil/Assets/Scripts/Player/PlayerInit.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class PlayerInit : MonoBehaviour
 {
@@ -9,11 +10,17 @@ public class PlayerInit : MonoBehaviour
     public static void SetPlayer(PlayerMetaData data) 
     {
         SceneManager.LoadScene(data.scene);
-        GameObject playerModel = GameObject.Find("obj_player");
         playerPos = new Vector3(data.position[0], data.position[1], data.position[2]);
-        playerModel.transform.position = playerPos;
+        SceneManager.sceneLoaded += OnSceneLoaded;
         PlayerHealth.initialHP = data.maxHP;
         PlayerHealth.currHP = data.currHP;
         PlayerCurrency.wealth = data.wealth;
     }
+
+    static void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+        GameObject playerModel = GameObject.Find("obj_player");
+        playerModel.transform.position = playerPos;
+    }
+// TODO : This may need to be made asynchronous but depending on if triggering this function on every scene load becomes a problem or not
+// if it does go back to trying to figure out LoadSceneAsycn and trying to set the player object on completion of that
 }
