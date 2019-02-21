e07ba60445d825db80674e72d517c8bea3a548df
Riley Robertson <goodsmellinguy@gmail.com>
2019-02-21 18:40:42 -0500

    Update to Character Select

--- Recoil/Assets/Scripts/Menus/CharacterSelect.cs
+++ Recoil/Assets/Scripts/Menus/CharacterSelect.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelect : MonoBehaviour
 {
     public GameObject obj_player;
+    public Button clickedButton;
     // Update is called once per frame
-    void changeSprite()
+    public void changeSprite()
     {
-        obj_player.player.GetComponent<SpriteRenderer>().sprite = null;
+        GameObject player = GameObject.Find("player");
+        SpriteRenderer rend = player.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+        rend.sprite = clickedButton.GetComponent<Image>().sprite;
+        Debug.Log("switched sprite to" + rend.sprite);
     }
 }
