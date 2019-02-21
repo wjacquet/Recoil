b7ac28fd2722400c2dbf64f3765fdd1fadd2354d
Walter Jacquette <wjacquet@purdue.edu>
2019-02-20 19:26:05 -0500

    Clean up data control

--- Recoil/Assets/Scripts/Misc/DataControl.cs
+++ Recoil/Assets/Scripts/Misc/DataControl.cs
@@ -12,12 +12,8 @@ public class DataControl : MonoBehaviour
     const string saveFile = "Save.dat";
     
     public static void Save() 
-    {
-        string folderPath = Path.Combine(Application.persistentDataPath, saveFolder);
-        if (!Directory.Exists (folderPath))
-            Directory.CreateDirectory (folderPath);            
-
-        string dataPath = Path.Combine(folderPath, saveFile);   
+    {          
+        string dataPath = BuildPath();   
         bool[] newUpgradeIndexes = new bool[SceneManager.sceneCountInBuildSettings];
 
         Array.Copy(PlayerInit.loadedUpgradesFound, newUpgradeIndexes, PlayerInit.loadedUpgradesFound.Length);
@@ -44,8 +40,7 @@ public class DataControl : MonoBehaviour
 
     public static void Load() 
     {
-        string folderPath = Path.Combine(Application.persistentDataPath, saveFolder);           
-        string dataPath = Path.Combine(folderPath, saveFile);
+        string dataPath = BuildPath();
 
         BinaryFormatter binaryFormatter = new BinaryFormatter();
         PlayerMetaData player;// = new PlayerMetaData();
@@ -70,11 +65,7 @@ public class DataControl : MonoBehaviour
 
     public static void NewGame() 
     {
-        string folderPath = Path.Combine(Application.persistentDataPath, saveFolder);
-        if (!Directory.Exists (folderPath))
-            Directory.CreateDirectory (folderPath);            
-
-        string dataPath = Path.Combine(folderPath, saveFile);   
+        string dataPath = BuildPath();   
         
         PlayerMetaData player = new PlayerMetaData(
                                     0,
@@ -89,5 +80,14 @@ public class DataControl : MonoBehaviour
         {
             binaryFormatter.Serialize (fileStream, player);
         }  
+    }
+
+    public static string BuildPath() 
+    {
+        string folderPath = Path.Combine(Application.persistentDataPath, saveFolder);
+        if (!Directory.Exists (folderPath))
+            Directory.CreateDirectory (folderPath);            
+
+        return Path.Combine(folderPath, saveFile);   
     }
 }
