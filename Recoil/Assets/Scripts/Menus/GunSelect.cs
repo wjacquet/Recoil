42fe5b45a79504ea7a82efcd26d3b083e6d733d3
Kyle Brown <brow1144@purdue.edu>
2019-02-26 17:33:34 -0500

    Basic array set up for selected guns

--- Recoil/Assets/Scripts/Menus/GunSelect.cs
+++ Recoil/Assets/Scripts/Menus/GunSelect.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class GunSelect : MonoBehaviour {
 
@@ -14,6 +15,9 @@ public class GunSelect : MonoBehaviour {
     public GameObject gunBox4;
     public GameObject gunBox5;
 
+    public string[] selectedGuns = new string[2];
+    public int currentSelection = 0;
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.G)) {
             if (gameIsPaused) {
@@ -22,6 +26,21 @@ public class GunSelect : MonoBehaviour {
                 Pause();
             }
         }
+    }
+
+    public void gunPressed() {
+
+        // Debug.Log(selectedGuns[0]);
+        selectedGuns[currentSelection] = EventSystem.current.currentSelectedGameObject.name;
+
+        Debug.Log(" ");
+        Debug.Log(selectedGuns[0]);
+        Debug.Log(selectedGuns[1]);
+        Debug.Log(" ");
+
+        if (currentSelection == 0) currentSelection = 1;
+        else currentSelection = 0;
+
     }
 
     public void Resume() {
