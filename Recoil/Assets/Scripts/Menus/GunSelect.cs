0281678ea8f8b8cf1ecf674b556dcad8da1d7897
Kyle Brown <brow1144@purdue.edu>
2019-02-26 10:47:02 -0500

    G to bring up gun selection

---
+++ Recoil/Assets/Scripts/Menus/GunSelect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunSelect : MonoBehaviour {
+
+    public static bool gameIsPaused = false;
+    public GameObject gunSelectUI;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.G)) {
+            if (gameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+    public void Resume() {
+        gunSelectUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause() {
+        Cursor.visible = true;
+        gunSelectUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
