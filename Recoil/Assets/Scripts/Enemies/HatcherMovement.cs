d5bc2fefbcaedf212f3d8bbf3e0e146ecfe5114c
Kyle Brown <brow1144@purdue.edu>
2019-03-01 11:13:38 -0500

    Walter plz help

--- Recoil/Assets/Scripts/Enemies/HatcherMovement.cs
+++ Recoil/Assets/Scripts/Enemies/HatcherMovement.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class HatcherMovement : MonoBehaviour {
     
     public GameObject hatcher;
+    GameObject player;
+    PlayerHealth playerHP;
     
     // Start is called before the first frame update
     void Start() {
-        
+        GameObject player = GameObject.Find("obj_player");
+        playerHP = player.GetComponent<PlayerHealth>();
     }
 
     // Update is called once per frame
     void Update() {
-        Debug.Log("MOVEEE");
+        player = GameObject.Find("obj_player");
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, playerPos, 75 * Time.deltaTime);    
     }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            playerHP.TakeDamage();
+        } 
+    }
+
 }
