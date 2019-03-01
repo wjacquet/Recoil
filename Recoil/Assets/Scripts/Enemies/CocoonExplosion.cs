9baf513b77e75de031fb864d3be7e97ac68f0f3a
Kyle Brown <brow1144@purdue.edu>
2019-03-01 09:50:48 -0500

    Cocoon now explodes!

---
+++ Recoil/Assets/Scripts/Enemies/CocoonExplosion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CocoonExplosion : MonoBehaviour {
+
+    // If the pod comes into contact with anything else
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Bullet" || collision.gameObject.tag == "Player") {
+            print("Kaboom !!!");
+            // Explode();
+            Destroy(gameObject);
+        }
+    }
+
+}
