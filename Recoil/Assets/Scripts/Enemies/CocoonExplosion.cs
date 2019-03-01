66991b62fcde70b0fbbc39ea8831b4c4629b86a7
Kyle Brown <brow1144@purdue.edu>
2019-03-01 10:10:18 -0500

    Hatcher now flys towards you when hatched

--- Recoil/Assets/Scripts/Enemies/CocoonExplosion.cs
+++ Recoil/Assets/Scripts/Enemies/CocoonExplosion.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class CocoonExplosion : MonoBehaviour {
 
+    public GameObject hatcher;
+
+
+    void Explode() {
+
+        StandardFireFunctions.FireAtPlayer(Instantiate(hatcher, transform.position, transform.rotation));
+
+    }
+
     // If the pod comes into contact with anything else
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Bullet" || collision.gameObject.tag == "Player") {
-            print("Kaboom !!!");
-            // Explode();
+            // print("Kaboom !!!");
             Destroy(gameObject);
+            Explode();
         }
     }
 
