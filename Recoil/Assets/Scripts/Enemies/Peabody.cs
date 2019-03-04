61af18ef834730cf922af00ae0143ab306d2927c
Kyle Brown <brow1144@purdue.edu>
2019-03-04 11:10:07 -0500

    Peabody now shoots

--- Recoil/Assets/Scripts/Enemies/Peabody.cs
+++ Recoil/Assets/Scripts/Enemies/Peabody.cs
@@ -7,21 +7,29 @@ public class Peabody : MonoBehaviour {
     public GameObject bullet;
     public GameObject player;
 
+    private GameObject proj;
+
+    public float timePerShot = 2.0f;
+
     // Start is called before the first frame update
     void Start() {
         StartCoroutine(PeabodyPattern());
     }
 
+    void Update() {
+        StandardFireFunctions.FireAtPlayer(proj);
+    }
+
     IEnumerator PeabodyPattern() {
         while (true) {
             yield return Shoot();
-            yield return new WaitForSeconds(2.0f);
+            yield return new WaitForSeconds(timePerShot);
+            Destroy(proj);
         }
     }
 
     IEnumerator Shoot() {
-        GameObject proj = Instantiate(bullet, transform.position, transform.rotation);
-        StandardFireFunctions.FireAtPlayer(proj);
+        proj = Instantiate(bullet, transform.position, transform.rotation);
         yield return null;
     }
 
