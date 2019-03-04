5c886ea7020a9429879529c0617fe19875a804d9
Nicholas Fordyce <FordyceNick@gmail.com>
2019-03-04 13:09:23 -0500

    Player sprite flips to face cursor

--- Recoil/Assets/Scripts/Player/PlayerMovement.cs
+++ Recoil/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,10 +6,26 @@ public class PlayerMovement : MonoBehaviour
 {
     protected Rigidbody2D rigidBody;
     public float speedLimit;
+    public SpriteRenderer mySpriteRenderer;
+    public GameObject gun;
     void Start() {
         rigidBody = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
         rigidBody.freezeRotation = true;
     }
+
+    void Update() {
+        GameObject cursor = GameObject.Find("obj_cursor");
+
+        // Check which direction to face sprite
+        if (cursor.transform.position.x <= transform.position.x) 
+            mySpriteRenderer.flipX = true;
+        else
+            mySpriteRenderer.flipX = false;
+        
+        // Make gun direction match player direction
+        gun.GetComponent<FireWeapon>().FlipGun(mySpriteRenderer.flipX);
+    }
+
     public void Recoil(Vector2 knockback) {
         rigidBody.velocity = rigidBody.velocity + knockback;
         if (rigidBody.velocity.magnitude > speedLimit) {
