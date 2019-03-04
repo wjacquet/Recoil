78e5dd35ef13de196eb1ad218254d4bb2b91c099
Kyle Brown <brow1144@purdue.edu>
2019-03-03 20:30:32 -0500

    Bug Gunner now shoots in the spread

---
+++ Recoil/Assets/Scripts/Enemies/BugGunner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugGunner : MonoBehaviour {
+
+    public GameObject bullet;
+    public GameObject player;
+ 
+    public int firstSpreadDegree = 25;
+    public int secondSpreadDegree = 15;
+
+    void Start() {
+        StartCoroutine(BugGunnerPattern());
+    }
+
+    IEnumerator BugGunnerPattern() {
+        while (true) {
+            yield return FirstSpread();
+            yield return new WaitForSeconds(1.0f);
+            yield return SecondSpread();
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    IEnumerator FirstSpread() {
+        StandardFireFunctions.FireAtPlayer(Instantiate(bullet, transform.position, transform.rotation));
+        StandardFireFunctions.FireDegreeOffsetFromPlayer(Instantiate(bullet, transform.position, transform.rotation), firstSpreadDegree);
+        StandardFireFunctions.FireDegreeOffsetFromPlayer(Instantiate(bullet, transform.position, transform.rotation), -firstSpreadDegree);
+        yield return null;
+    }
+
+    IEnumerator SecondSpread() {
+        StandardFireFunctions.FireDegreeOffsetFromPlayer(Instantiate(bullet, transform.position, transform.rotation), secondSpreadDegree);
+        StandardFireFunctions.FireDegreeOffsetFromPlayer(Instantiate(bullet, transform.position, transform.rotation), -secondSpreadDegree);
+        yield return null;   
+        }
+    
+}
