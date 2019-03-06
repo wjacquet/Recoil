f48519fda7c100a1c9ebcb4e1764b8776c00c264
Kyle Brown <brow1144@purdue.edu>
2019-03-06 15:37:22 -0500

    Basic pot done
    NOw to do movement

---
+++ Recoil/Assets/Scripts/Enemies/Greenhouse/PotPatroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotPatroller : MonoBehaviour {
+
+    public GameObject bullet;
+
+    public float timeBetweenShoots = 2.0f;
+    
+    void Start() {
+        StartCoroutine(PotFirePattern());
+    }
+
+    IEnumerator PotFirePattern() {
+        while (true) {
+            yield return Shoot();
+            yield return new WaitForSeconds(timeBetweenShoots);
+        }
+    }
+    
+    IEnumerator Shoot() {
+        GameObject proj = Instantiate(bullet, transform.position, transform.rotation);
+        StandardFireFunctions.FireAtPlayer(proj);
+
+        yield return null;
+    }
+}
