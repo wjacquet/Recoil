a5af015ef69f6caaa521e1eee02b768fa610fbec
Kyle Brown <kb16brown@gmail.com>
2019-03-17 19:01:33 -0400

    Magnet ui set up

---
+++ Recoil/Assets/Scripts/Abilites/MagnetAbility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetAbility : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            PlayerHealth playerHP = GameObject.Find("obj_player").GetComponent<PlayerHealth>(); 
+            // playerHP.UpgradeHP();
+            Destroy(gameObject);
+        }
+    }
+}
