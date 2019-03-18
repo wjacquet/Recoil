c9e681b55853a16e3116ba3ab8c865ac95341496
Nicholas Fordyce <FordyceNick@gmail.com>
2019-03-18 14:57:03 -0400

    Create fan block animation

---
+++ Recoil/Assets/Scripts/Misc/FanController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanController : MonoBehaviour
+{
+    Transform blades;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blades = gameObject.transform.GetChild(0);
+        print("BLADES");
+        print(blades);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        blades.Rotate(0.0f, 0.0f, -5.0f);
+    }
+}
