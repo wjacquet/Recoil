d9d1a40b9df78d77e4b78b3fdfbf0f6bb95ce1c8
Kyle Brown <kb16brown@gmail.com>
2019-03-18 20:00:57 -0400

    Ray shooting horizontal

--- Recoil/Assets/Scripts/Enemies/Greenhouse/LaserCrystal.cs
+++ Recoil/Assets/Scripts/Enemies/Greenhouse/LaserCrystal.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserCrystal : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class LaserCrystal : MonoBehaviour {
+
+    LineRenderer line;
+
+    void Start() {
+        line = gameObject.GetComponent<LineRenderer>();
+        line.enabled = false;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            StopCoroutine("FireLaser");
+            StartCoroutine("FireLaser");
+        }
+    }
+
+    IEnumerator FireLaser(){
+        line.enabled = true;
+ 
+        while (Input.GetButton("Fire1")) {
+            Ray2D ray = new Ray2D(transform.position, transform.right);
+            RaycastHit2D hit;
+
+            line.SetPosition(0, ray.origin);
+
+            hit = Physics2D.Raycast(ray.origin, Vector2.right, 200);
+            if (hit.collider){
+                line.SetPosition(1, hit.point);
+            } else {
+                line.SetPosition(1, ray.GetPoint(200));
+            }
+            
+            yield return null;
+        }
+
+        line.enabled = false;
     }
 }
