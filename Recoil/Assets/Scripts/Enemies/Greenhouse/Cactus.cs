68500c97193ad4eac7c1c306e4d0f94f65ad85bb
Kyle Brown <brow1144@purdue.edu>
2019-03-20 13:42:37 -0400

    Enemy almost working

---
+++ Recoil/Assets/Scripts/Enemies/Greenhouse/Cactus.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cactus : MonoBehaviour {
+
+    private GameObject player; 
+    private PlayerHealth playerHP;
+
+    void Start() {           
+        player = GameObject.Find("obj_player");
+        playerHP = player.GetComponent<PlayerHealth>();
+
+        StartCoroutine(CactusPattern());
+    }
+
+    IEnumerator CactusPattern() {
+        while (true) {
+            yield return Jump();
+            yield return Stop();
+            yield return Fall();
+            yield return Stop();
+        }
+    }
+
+    // // Update is called once per frame
+    IEnumerator Jump() {
+        StandardFireFunctions.FireVetically(gameObject);
+
+        yield return new WaitForSeconds(0.8f);
+    }
+
+    IEnumerator Stop() {
+        StandardFireFunctions.StopFire(gameObject);
+
+        yield return new WaitForSeconds(0.8f);
+    }
+
+    IEnumerator Fall() {
+        StandardFireFunctions.FireDown(gameObject);
+
+        yield return new WaitForSeconds(0.8f);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            playerHP.TakeDamage();   
+        }
+    }
+
+}
