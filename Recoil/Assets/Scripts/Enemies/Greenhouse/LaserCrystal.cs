3a082852c129bac87c3bb16f6354dd7b6212f084
Kyle Brown <kb16brown@gmail.com>
2019-03-19 20:31:53 -0400

    All lazers firing

--- Recoil/Assets/Scripts/Enemies/Greenhouse/LaserCrystal.cs
+++ Recoil/Assets/Scripts/Enemies/Greenhouse/LaserCrystal.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class LaserCrystal : MonoBehaviour {
 
-    private PlayerHealth playerHP;
-    LineRenderer line;
+    public LineRenderer line;
+    public LineRenderer line1;
+    public LineRenderer line2;
+    public LineRenderer line3;
 
-    private Vector2 rotateVector = new Vector2(0, 1); 
+    private PlayerHealth playerHP;
+
+    private Vector2 rotateVector = new Vector2(0, 0); 
+    private Vector2 rotateVector1 = new Vector2(0, 0); 
+    private Vector2 rotateVector2 = new Vector2(0, 0); 
+    private Vector2 rotateVector3 = new Vector2(0, 0); 
+    
     private int angle = 0;
+    private int angle1 = 90;
+    private int angle2 = 180;
+    private int angle3 = 270;
 
 
     void Start() {
         GameObject player = GameObject.Find("obj_player");
         playerHP = player.GetComponent<PlayerHealth>();
-
-        line = gameObject.GetComponent<LineRenderer>();
-        line.enabled = false;
     }
 
     void Update() {
@@ -24,20 +32,45 @@ public class LaserCrystal : MonoBehaviour {
     }
 
     void FireLaser(){
-        line.enabled = true;
  
         Ray2D ray = new Ray2D(transform.position, transform.right);
+        Ray2D ray1 = new Ray2D(transform.position, transform.right);
+        Ray2D ray2 = new Ray2D(transform.position, transform.right);
+        Ray2D ray3 = new Ray2D(transform.position, transform.right);
+
+        
         RaycastHit2D[] hits;
+        RaycastHit2D[] hits1;
+        RaycastHit2D[] hits2;
+        RaycastHit2D[] hits3;
+        
 
-        Debug.Log(angle);
         rotateVector = Quaternion.AngleAxis(angle++, Vector3.forward) * Vector3.right;
-        hits = Physics2D.RaycastAll(ray.origin, rotateVector, 20000);
-        if (angle >= 360)
-            angle = 0;
+        rotateVector1 = Quaternion.AngleAxis(angle1++, Vector3.forward) * Vector3.right;
+        rotateVector2 = Quaternion.AngleAxis(angle2++, Vector3.forward) * Vector3.right;
+        rotateVector3 = Quaternion.AngleAxis(angle3++, Vector3.forward) * Vector3.right;
 
+        hits = Physics2D.RaycastAll(ray.origin, rotateVector, 20000);
+        hits1 = Physics2D.RaycastAll(ray1.origin, rotateVector1, 20000);
+        hits2 = Physics2D.RaycastAll(ray2.origin, rotateVector2, 20000);
+        hits3 = Physics2D.RaycastAll(ray3.origin, rotateVector3, 20000);
+        
+        angle = ResetAngle(angle);
+        angle1 = ResetAngle(angle1);
+        angle2 = ResetAngle(angle2);
+        angle3 = ResetAngle(angle3);
+
+        
+        HandleCollisions(hits, line, ray);
+        HandleCollisions(hits1, line1, ray1);
+        HandleCollisions(hits2, line2, ray2);
+        HandleCollisions(hits3, line3, ray3);
+    }
+
+    void HandleCollisions(RaycastHit2D[] hits, LineRenderer line,  Ray2D ray) {
 
         line.SetPosition(0, ray.origin);
-        
+
         for (int i = 0; i < hits.Length; ++i) {
             if (!(hits[i].collider.gameObject.tag == "Enemy")) {
                 if (hits[i].collider.gameObject.tag == "Player") {
@@ -52,8 +85,11 @@ public class LaserCrystal : MonoBehaviour {
                 line.SetPosition(1, ray.GetPoint(20000));
             }
         }
+    }
 
-        // yield return null;
-
+    int ResetAngle(int angle) {
+        if (angle >= 360)
+            angle = 0;
+        return angle;
     }
 }
