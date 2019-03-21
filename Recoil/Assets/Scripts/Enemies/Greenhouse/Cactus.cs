881235d7b907a0010d7b59c951d792fe3857357e
Kyle Brown <brow1144@purdue.edu>
2019-03-21 13:08:40 -0400

    Cactus wroking how nick wanted it to now

--- Recoil/Assets/Scripts/Enemies/Greenhouse/Cactus.cs
+++ Recoil/Assets/Scripts/Enemies/Greenhouse/Cactus.cs
@@ -7,44 +7,61 @@ public class Cactus : MonoBehaviour {
     private GameObject player; 
     private PlayerHealth playerHP;
 
+    public float stopWaitTime = 0.2f;
+    public float timeBetweenSpeedChanges = 0.1f;
+
+    public float startingSpeed = 100.0f;
+    
+    private float slowDownSpeed;
+    private float speedUpSpeed;
+
     void Start() {           
         player = GameObject.Find("obj_player");
         playerHP = player.GetComponent<PlayerHealth>();
+
+        slowDownSpeed = startingSpeed / 10;
+        speedUpSpeed = (startingSpeed / 10) + 4;
 
         StartCoroutine(CactusPattern());
     }
 
     IEnumerator CactusPattern() {
-        while (true) {
-            yield return Jump();
-            yield return Stop();
-            yield return Fall();
-            yield return Stop();
+        for (int i = 0; i < 10; i++) {
+            yield return SlowDown();
         }
+    
+        yield return Stop();
+        
+        for (int i = 0; i < 15; i++) {
+            yield return SpeedUp();
+        }
+
+        yield return Stop();
     }
 
-    // // Update is called once per frame
-    IEnumerator Jump() {
-        StandardFireFunctions.FireVetically(gameObject);
+    IEnumerator SlowDown() {
+        StandardFireFunctions.FireVerticallyFakeGravity(gameObject, startingSpeed);
+        startingSpeed = startingSpeed - slowDownSpeed;
+        yield return new WaitForSeconds(timeBetweenSpeedChanges);
+    }   
 
-        yield return new WaitForSeconds(0.8f);
-    }
+    IEnumerator SpeedUp() {
+        StandardFireFunctions.FireDownFakeGravity(gameObject, startingSpeed);
+        startingSpeed = startingSpeed + speedUpSpeed;
+        yield return new WaitForSeconds(timeBetweenSpeedChanges);
+    }  
 
     IEnumerator Stop() {
         StandardFireFunctions.StopFire(gameObject);
 
-        yield return new WaitForSeconds(0.8f);
-    }
-
-    IEnumerator Fall() {
-        StandardFireFunctions.FireDown(gameObject);
-
-        yield return new WaitForSeconds(0.8f);
+        yield return new WaitForSeconds(stopWaitTime);
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
             playerHP.TakeDamage();   
+        } else if (collision.gameObject.tag == "Enemy") {
+            Destroy(gameObject);
         }
     }
 
