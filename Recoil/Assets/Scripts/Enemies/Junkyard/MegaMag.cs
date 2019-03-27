495c0a5f507879bb958d25044a870d82cf6d4367
Riley Robertson <goodsmellinguy@gmail.com>
2019-03-27 16:56:29 -0400

    Sprider complete

--- Recoil/Assets/Scripts/Enemies/Junkyard/MegaMag.cs
+++ Recoil/Assets/Scripts/Enemies/Junkyard/MegaMag.cs
@@ -22,7 +22,6 @@ public class MegaMag : MonoBehaviour
         player = GameObject.Find("obj_player");
         Vector2 magPos = new Vector2(transform.position.x, transform.position.y);
         float currAngle = Mathf.Atan2(player.transform.position.y - transform.position.y, player.transform.position.x - transform.position.x) * 180 / Mathf.PI;
-        Debug.Log(currAngle);
         if (Vector2.Distance(magPos, player.transform.position) < (range*16)) {
             if ( currAngle < angle+ spread && currAngle > angle - spread) {
                 player.transform.position = Vector2.MoveTowards(player.transform.position, magPos, 50 * Time.deltaTime);
