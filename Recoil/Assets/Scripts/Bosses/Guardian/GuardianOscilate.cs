5947e1f6dd71749c11243bb4de34a47a0963f3a1
Riley Robertson <goodsmellinguy@gmail.com>
2019-03-28 23:15:50 -0400

    Guardian/Dragon fully functional
    Both now destroy blocking walls on death

---
+++ Recoil/Assets/Scripts/Bosses/Guardian/GuardianOscilate.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardianOscilate : MonoBehaviour
+{
+    public float speed;
+    public LayerMask enemyMask;
+    private Rigidbody2D body;
+    private Transform trans;
+    private float width;
+
+    public bool floor = true;
+    public bool cieling = false;
+    public bool right = false;
+    public bool left = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();
+        trans = this.transform;
+        SpriteRenderer sprit = gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        width = trans.GetChild(0).GetComponent<SpriteRenderer>().bounds.extents.x;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 lineCastPos = trans.position - trans.right * width;
+        Vector3 lineCastV3 = lineCastPos;
+        Vector2 transRightV2 = trans.right;
+
+        bool isGrounded;
+        
+        Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down * 40);
+        isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down * 40, enemyMask);
+        //Debug.Log("IsGrounded: " + isGrounded);
+        
+
+        bool isBlocked = Physics2D.Linecast(lineCastPos, lineCastPos - transRightV2, enemyMask);
+        //Debug.Log("IsBlocked: " + isBlocked);
+
+        Debug.DrawLine(lineCastPos, lineCastPos + transRightV2);
+
+        
+        //Debug.Log("IsGround: " + isGrounded + "IsBlocked: " + isBlocked);
+
+        if (!isGrounded || isBlocked)
+        {
+        //Debug.Log("IsGround: " + isGrounded + "IsBlocked: " + isBlocked);
+        Vector3 currRotation = trans.eulerAngles;
+        currRotation.y += 180;
+        trans.eulerAngles = currRotation;
+        }
+
+        Vector2 vel = body.velocity;
+        vel.x = -trans.right.x * speed;
+        body.velocity = vel;
+        Debug.Log(body.velocity);
+        
+    }
+}
