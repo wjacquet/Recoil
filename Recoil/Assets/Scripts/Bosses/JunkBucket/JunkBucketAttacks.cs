2139bcdb2deefe40b6522291bb27ccd1d3dff7b9
Walter Jacquette <wjacquet@purdue.edu>
2019-03-28 23:44:12 -0400

    Start Final Attack Procedures

--- Recoil/Assets/Scripts/Bosses/JunkBucket/JunkBucketAttacks.cs
+++ Recoil/Assets/Scripts/Bosses/JunkBucket/JunkBucketAttacks.cs
@@ -8,33 +8,96 @@ public class JunkBucketAttacks : MonoBehaviour
     public GameObject projectile;
     public GameObject enemySpawn;
 
-    private bool shouldScorchers = false;
+    // private bool shouldScorchers = false;
+    private bool newAttack = false;
+    private bool allowStream = false;
     private bool shot = false;
 
+    private int firstSpreadDegree = 25;
+    private int secondSpreadDegree = 15;
+
+    private int atkSelector;
     private Vector3 projectileSpawnPos = new Vector3(288.5f, 341f, 0f);
 
-    void Start() 
+    void OnEnable()
     {
-        //shootPhoton();
+        StartCoroutine(startScorch());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (!shot) shootPhoton();
-        shot = true;
+        if (newAttack) {
+            atkSelector = Random.Range(0, 5);
+
+            switch(atkSelector) {
+                case 0:
+                    StartCoroutine(startStream());
+                    break;
+                case 1:
+                    StartCoroutine(startScorch());
+                    break;
+                case 2:
+                    StartCoroutine(shootPhoton());
+                    break;
+                case 3:
+                    StartCoroutine(startStream());
+                    break;
+                case 4:
+                    StartCoroutine(startStream());
+                    break;
+            }
+
+            newAttack = false;
+        }
+
+        if (allowStream) 
+        {
+            shootStream();
+        }
     }
 
-
-    public void shootPhoton() 
-    {        
-        GameObject proj = Instantiate(photonShot, projectileSpawnPos, transform.rotation);
+    public void shootStream() 
+    {
+        GameObject proj = Instantiate(projectile, projectileSpawnPos, transform.rotation);
         StandardFireFunctions.FireAtPlayer(proj);
     }
 
-    public void startScorch() 
+    IEnumerator startStream() 
+    {
+        allowStream = true;
+        yield return new WaitForSeconds(2.0f);
+        newAttack = true;
+        yield return new WaitForSeconds(1.0f);
+        allowStream = false;
+    }
+
+    IEnumerator shootPhoton() 
+    {        
+        GameObject proj = Instantiate(photonShot, projectileSpawnPos, transform.rotation);
+        StandardFireFunctions.FireAtPlayerWithSetSpeed(proj, 120);    
+        yield return new WaitForSeconds(2.0f);
+        newAttack = true;
+        yield return new WaitForSeconds(1.0f);
+    }
+
+    IEnumerator startScorch() 
     {
         JunkScorchers.shouldFlame = true;
+        yield return new WaitForSeconds(2.0f);
+        newAttack = true;
+        yield return new WaitForSeconds(1.0f);
+        JunkScorchers.shouldFlame = false;
+    }
+
+
+    
+    IEnumerator FirstSpread() {
+        StandardFireFunctions.FireAtPlayer(Instantiate(projectile, projectileSpawnPos, transform.rotation));
+        StandardFireFunctions.FireDegreeOffsetFromPlayer(Instantiate(projectile, projectileSpawnPos, transform.rotation), firstSpreadDegree);
+        StandardFireFunctions.FireDegreeOffsetFromPlayer(Instantiate(projectile, projectileSpawnPos, transform.rotation), -firstSpreadDegree);
+        yield return new WaitForSeconds(1.5f);
+        StandardFireFunctions.FireDegreeOffsetFromPlayer(Instantiate(projectile, projectileSpawnPos, transform.rotation), secondSpreadDegree);
+        StandardFireFunctions.FireDegreeOffsetFromPlayer(Instantiate(projectile, projectileSpawnPos, transform.rotation), -secondSpreadDegree);
     }
 }
