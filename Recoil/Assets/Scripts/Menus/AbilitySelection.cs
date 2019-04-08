f1e9ff042fa3210cade85a1f6113af2ee50f63d4
Kyle Brown <brow1144@purdue.edu>
2019-04-08 17:29:14 -0400

    UI for abilities complete and funcitoning

--- Recoil/Assets/Scripts/Menus/AbilitySelection.cs
+++ Recoil/Assets/Scripts/Menus/AbilitySelection.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
 
 public class AbilitySelection : MonoBehaviour {
 
     public GameObject abilityBox0;
     public GameObject abilityBox1;
 
-    void Update() {
+    public Image selectedImage;
+    public Sprite magnetSprite;
+    public Sprite flowerSprite;
 
-        if (SelectionMenus.abilitiesOpen) 
-            if (SelectionMenus.gameIsPaused) 
-                ShowAbilitesUnlocked();
+    public static string currentAbility = "";
+
+    void Update() {
+        // if (SelectionMenus.abilitiesOpen) 
+        //     if (SelectionMenus.gameIsPaused) 
+                // ShowAbilitesUnlocked();
     }
 
     void ShowAbilitesUnlocked() {
@@ -23,5 +31,25 @@ public class AbilitySelection : MonoBehaviour {
 
         abilityBox0.SetActive(magnet);
         abilityBox1.SetActive(flower);
+    }
+
+    void updateCurrentSelection() {
+        if (currentAbility == "magnet")
+            selectedImage.sprite = magnetSprite;
+        else if (currentAbility == "flower")
+            selectedImage.sprite = flowerSprite;
+    }
+
+
+    public void magnetClicked() {
+        currentAbility = "magnet";
+
+        updateCurrentSelection();
+    }
+
+    public void flowerClicked() {
+        currentAbility = "flower";
+
+        updateCurrentSelection();
     }
 }
--- Recoil/Assets/Scripts/Misc/DataControl.cs
+++ Recoil/Assets/Scripts/Misc/DataControl.cs
@@ -85,8 +85,8 @@ public class DataControl : MonoBehaviour
                                     new bool[6] {true, false, false, false, false, false},
                                     new int[2] {0, 0},
                                     0,
-                                    true,
-                                    true);
+                                    false,
+                                    false);
 
         
         BinaryFormatter binaryFormatter = new BinaryFormatter();
