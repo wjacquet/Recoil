c932407695968f1ba6c7cd053173aa87646fb69d
Kyle Brown <brow1144@purdue.edu>
2019-04-10 11:20:56 -0400

    Can now buy guns and have them remove from the store

--- Recoil/Assets/Scripts/Menus/Store.cs
+++ Recoil/Assets/Scripts/Menus/Store.cs
@@ -5,24 +5,43 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
 
-// TODO Allow them to buy 
-    // Should subtract from their coins
-    // Should add to health / show gun in gun selection
-
 // TODO Only Show the guns not already bought
+
+// TODO Allow them to buy 
+    // Should add to health 
 public class Store : MonoBehaviour {
 
     public GameObject player;
-   
-    // void Update() {
-    //     if (SelectionMenus.storeOpen) 
-    //         if (SelectionMenus.gameIsPaused) 
-    //             showAvailableStoreItems();
-    // }
+    
+    public GameObject box1;
+    public GameObject box2;
+    public GameObject box3;
+    public GameObject box4;
+    public GameObject box5;
+    public GameObject box6;
 
-    // void showAvailableStoreItems() {
+    private GameObject[] gunBoxes;
 
-    // }
+    void Start() {
+        gunBoxes = new GameObject[6] {null, box1, box2, box3, box4, box5};
+
+    }
+
+    void Update() {
+        if (SelectionMenus.storeOpen) 
+            if (SelectionMenus.gameIsPaused) 
+                showAvailableStoreItems();
+    }
+
+    void showAvailableStoreItems() {
+        for (int i = 1; i < gunBoxes.Length; i++) {
+            if (PlayerInit.gunsFound[i]) {
+                gunBoxes[i].SetActive(false);
+            } else {
+                gunBoxes[i].SetActive(true);
+            }
+        }
+    }
 
     public void itemSelected() {
         string indexStr = EventSystem.current.currentSelectedGameObject.name;
