1a7cab8cf63444ed1ec6a1318deabf682b4ac5f1
Kyle Brown <brow1144@purdue.edu>
2019-04-11 09:37:10 -0400

    Basic black hole complete

---
+++ Recoil/Assets/Scripts/Enemies/Moon/BlackHole.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour {
+
+    GameObject player;
+
+    void Start() {
+        player = GameObject.Find("obj_player");
+    }
+
+
+    void Update() {
+        
+        // player = GameObject.Find("obj_player");
+        Vector2 magPos = new Vector2(transform.position.x, transform.position.y);
+
+        if (Vector2.Distance(player.transform.position, magPos) < 50) {
+            player.transform.position = Vector2.MoveTowards(player.transform.position, magPos, 50 * Time.deltaTime);
+        }
+
+    }
+}
