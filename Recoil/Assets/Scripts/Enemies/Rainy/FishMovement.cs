520f8df90a35bab6a0116c2922f807a8e83f0400
Kyle Brown <brow1144@purdue.edu>
2019-04-11 10:36:46 -0400

    Basic fish uploaded

---
+++ Recoil/Assets/Scripts/Enemies/Rainy/FishMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMovement : MonoBehaviour {
+
+    GameObject player;
+    PlayerHealth playerHP;
+
+    private Vector2 center;
+    private int distance = 1;
+    private bool left = false;
+
+    void Start() {
+        player = GameObject.Find("obj_player");
+        playerHP = player.GetComponent<PlayerHealth>();
+
+        center = transform.position;
+    }
+
+    void Update() {
+        Fly(distance);
+        
+        if (left) distance++;
+        else distance--;
+    }
+
+    void Fly(int distance) { 
+        var offset = new Vector2(distance, 0);
+        transform.position = offset;
+    }
+
+    void FlipFish() {
+        Debug.Log("FLIPPING FISH");
+        left = !left;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log(LayerMask.LayerToName(collision.gameObject.layer));
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player") {
+            playerHP.TakeDamage();   
+        } 
+        
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Ground") {
+            FlipFish();
+        } 
+    }
+}
