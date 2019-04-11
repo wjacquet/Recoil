04327fc7f73131a55cdf07f814c2a3d11da09224
Kyle Brown <brow1144@purdue.edu>
2019-04-11 13:50:49 -0400

    Flying Fish complete

--- Recoil/Assets/Scripts/Enemies/Rainy/FishShooting.cs
+++ Recoil/Assets/Scripts/Enemies/Rainy/FishShooting.cs
@@ -5,36 +5,21 @@ using UnityEngine;
 public class FishShooting : MonoBehaviour {
 
     public GameObject bullet;
-    public float timeBetweenShoots = 2.0f;
-
-    private float speed = 1.0f;
-    GameObject proj;
-    GameObject proj1;
+    public float timeBetweenShoots = 1.0f;
 
     void Start() {
-        proj = Instantiate(bullet, transform.position, transform.rotation);
-        proj1 = Instantiate(bullet, transform.position, transform.rotation);
+        StartCoroutine(FishFirePattern());
     }
 
-    void Update() {
-        if (proj == null && proj1 == null) {
-            proj = Instantiate(bullet, transform.position, transform.rotation);
-            proj1 = Instantiate(bullet, transform.position, transform.rotation);
-            speed = 1.0f;
-        } else {
-            Debug.Log(proj);
-            if (proj != null) ShootUp();
-            if (proj1 != null) ShootDown();
+    IEnumerator FishFirePattern() {
+        while(true) {
+            yield return LoadBullet();
         }
     }
-  
-    void ShootUp() {
-        StandardFireFunctions.FireVerticallyFakeGravity(proj, speed);
-        speed = speed + 0.4f;
-    }
 
-    void ShootDown() {
-        StandardFireFunctions.FireDownFakeGravity(proj1, speed);
-        speed = speed + 0.4f;
+    IEnumerator LoadBullet() {
+        Instantiate(bullet, transform.position, transform.rotation).SendMessage("FireUp");
+        Instantiate(bullet, transform.position, transform.rotation).SendMessage("FireDown");
+        yield return new WaitForSeconds(timeBetweenShoots);
     }
 }
