5dff8e1f71478cb2ede9bec3df0db06dcde07701
Kyle Brown <brow1144@purdue.edu>
2019-04-12 13:18:11 -0400

    Jellyfish set up

---
+++ Recoil/Assets/Scripts/Enemies/Rainy/Jellyfish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jellyfish : MonoBehaviour {
+    
+    GameObject player;
+    PlayerHealth playerHP;
+
+    void Start() {
+        player = GameObject.Find("obj_player");
+        playerHP = player.GetComponent<PlayerHealth>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player") {
+            playerHP.TakeDamage();   
+        }
+    }
+
+}
