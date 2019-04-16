654a87a7b666d2c10ace99fdb2ffec95ddb75392
Kyle Brown <brow1144@purdue.edu>
2019-04-16 11:42:59 -0400

    Urchin moveement done

---
+++ Recoil/Assets/Scripts/Enemies/Rainy/UrchinMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UrchinMovement : MonoBehaviour {
+
+    GameObject player;
+    PlayerHealth playerHP;
+
+    public Rigidbody2D rigidBody;
+    public int fractionSpeed = 1/25;
+    public float timeBetween = 1.0f;
+
+    private Vector2 center;
+
+    void Start() {
+        player = GameObject.Find("obj_player");
+        playerHP = player.GetComponent<PlayerHealth>();
+
+        center = transform.position;
+        StartCoroutine(HoverPattern());
+        rigidBody = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+    }
+
+
+    IEnumerator HoverPattern() {
+        while (true) {
+            yield return Hover(180);
+            yield return Hover(-180);
+        }
+    }
+
+    IEnumerator Hover(int yValue) {
+        // Debug.Log(Vector2.Distance(transform.position, center));
+        Vector2 movement = new Vector2(0, yValue);
+        rigidBody.velocity = movement * fractionSpeed;
+
+        yield return new WaitForSeconds(timeBetween);
+    }
+
+     void OnTriggerEnter2D(Collider2D collision) {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player") {
+            playerHP.TakeDamage();   
+        } 
+    }
+
+}
