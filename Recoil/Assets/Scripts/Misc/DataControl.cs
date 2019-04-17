60d37846140e1a0acdc855a8a04ffa853d9f77ec
Kyle Brown <brow1144@purdue.edu>
2019-04-17 09:49:10 -0400

    Only one menu at a time now

--- Recoil/Assets/Scripts/Misc/DataControl.cs
+++ Recoil/Assets/Scripts/Misc/DataControl.cs
@@ -80,7 +80,7 @@ public class DataControl : MonoBehaviour
         PlayerMetaData player = new PlayerMetaData(
                                     10,
                                     10,
-                                    100000,
+                                    0,
                                     "Scenes/GrassyZoneScenes/GrassyZoneEntrance",
                                     new[] { 9.6f, -4.1f, 0f },
                                     new bool[SceneManager.sceneCountInBuildSettings],
