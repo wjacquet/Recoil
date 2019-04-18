e2617c32c3853147badd5a2edf76313083248e16
Kyle Brown <brow1144@purdue.edu>
2019-04-18 09:27:14 -0400

    Basic snake set up

--- Recoil/Assets/Scripts/Enemies/Rainy/SnakeMovement.cs
+++ Recoil/Assets/Scripts/Enemies/Rainy/SnakeMovement.cs
@@ -11,76 +11,56 @@ public class SnakeMovement : MonoBehaviour {
     public int fractionSpeed = 25;
     public float timeBetween = 1.0f;
 
-    // private Vector2 center;
     private bool left;
 
     float amplitudeX = 20.0f;
-    float amplitudeY = 20.0f;
+    float amplitudeY = 50.0f;
     float omegaX = 5.0f;
     float omegaY = 5.0f;
     float index;
 
-    private Vector2 center;
+    private bool flip = false;
 
     void Start() {
         player = GameObject.Find("obj_player");
         playerHP = player.GetComponent<PlayerHealth>();
 
-        // StartCoroutine(HoverPattern());
         rigidBody = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
-        center = transform.localPosition;
     }
 
     void Update() {
-        if (Mathf.Abs(center.x - transform.localPosition.x) <= 50) {
-            MoveRight();
+        if (!flip) {
+            MoveRight(Mathf.PI * 10);
         } else {
-            MoveLeft();
+            MoveRight(-(Mathf.PI * 10));
         }
     }
 
-    void MoveRight() {
+    void MoveRight(float x) {
         index += Time.deltaTime;
-        float x = 3.14159f * amplitudeX * index;
-        float y = (amplitudeY * Mathf.Sin(omegaY * index));
-        transform.localPosition = new Vector2(x, y);
+        float y;
+
+        y = amplitudeY * Mathf.Sin(omegaY * index);
+
+        Vector2 direction = new Vector2(x, y);
+        float speed = 0.8f;
+
+        rigidBody.velocity = direction * speed;
     }
 
-    void MoveLeft() {
-        index += Time.deltaTime;
-        float x = -3.14159f * amplitudeX * index;
-        float y = (amplitudeY * Mathf.Sin(omegaY * index));
-        transform.localPosition = new Vector2(x, y);
+    void FlipSnake() {
+        flip = !flip;
     }
 
-    // void Update() {        
-    //     if (left) {
-    //         StandardFireFunctions.FireLeft(gameObject, 30.0f);
-    //         gameObject.transform.localScale = new Vector2(1, gameObject.transform.localScale.y);        
-    //     } else {
-    //         StandardFireFunctions.FireRight(gameObject, 30.0f);
-    //         gameObject.transform.localScale = new Vector2(-1, gameObject.transform.localScale.y);        
-    //     }
-    // }
-
-    // IEnumerator HoverPattern() {
-    //     while (true) {
-    //         yield return Hover(180);
-    //         yield return Hover(-180);
-    //     }
-    // }
-
-    // IEnumerator Hover(int yValue) {
-    //     Vector2 movement = new Vector2(0, yValue);
-    //     rigidBody.velocity = movement * 1/fractionSpeed;
-
-    //     yield return new WaitForSeconds(timeBetween);
-    // }
-     
     void OnTriggerEnter2D(Collider2D collision) {
         if (LayerMask.LayerToName(collision.gameObject.layer) == "Player") {
             playerHP.TakeDamage();   
         } 
+
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Ground") {
+            FlipSnake();
+        } 
     }
 
 }
+    
