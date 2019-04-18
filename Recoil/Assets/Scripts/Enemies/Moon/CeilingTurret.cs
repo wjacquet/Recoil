159ccbdfc46d5e083e9e02f94241143ef9c532f8
Walter Jacquette <wjacquet@purdue.edu>
2019-04-18 17:50:44 -0400

    Chaser rotates and Is more Deadly

--- Recoil/Assets/Scripts/Enemies/Moon/CeilingTurret.cs
+++ Recoil/Assets/Scripts/Enemies/Moon/CeilingTurret.cs
@@ -5,27 +5,29 @@ using UnityEngine;
 public class CeilingTurret : MonoBehaviour
 {
     public GameObject bullet;
-    public int recharge;
-    private int rechargeCounter = 0;
+    private bool firing = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (rechargeCounter > 0) {
-            rechargeCounter--;
-        }
-
-        // Fire on mouse click and reset reloadTimer
-        if (rechargeCounter == 0 && GetVectorToPlayer().y <= 20) {
-            rechargeCounter = recharge;
-            Shoot();
+        if (!firing && GetVectorToPlayer().y <= 20) {
+            StartCoroutine(Shoot());
         }
     }
 
-    void Shoot()
+    IEnumerator Shoot()
     {
+        firing = true;
         GameObject proj = Instantiate(bullet, transform.position, transform.rotation);
         StandardFireFunctions.FireAtPlayer(proj);
+        yield return new WaitForSeconds(0.8f);
+        proj = Instantiate(bullet, transform.position, transform.rotation);
+        StandardFireFunctions.FireDegreeOffsetFromPlayer(proj, 8);
+        yield return new WaitForSeconds(0.8f);
+        proj = Instantiate(bullet, transform.position, transform.rotation);
+        StandardFireFunctions.FireDegreeOffsetFromPlayer(proj, -8);
+        yield return new WaitForSeconds(2.5f);
+        firing = false;
     }
 
     Vector2 GetVectorToPlayer() 
@@ -34,9 +36,4 @@ public class CeilingTurret : MonoBehaviour
         GameObject player = GameObject.Find("obj_player");
         return player.transform.position - transform.position;
     }
-
-    // IEnumerator StaggeredFire() 
-    // {
-
-    // }
 }
--- Recoil/Assets/Scripts/Enemies/Moon/Chaser.cs
+++ Recoil/Assets/Scripts/Enemies/Moon/Chaser.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HatcherMovement : MonoBehaviour {
-    
+public class Chaser : MonoBehaviour
+{
     public GameObject hatcher;
     GameObject player;
     PlayerHealth playerHP;
     Rigidbody2D rigidBody;
 
     private bool jump = false;
+    private bool rotating = false;
+    private Vector3 rotationVector = new Vector3(0,0,1);
 
     public int speed = 70;
     
@@ -30,12 +32,17 @@ public class HatcherMovement : MonoBehaviour {
         // Move towards player
         if (!jump)  
             StandardFireFunctions.FireAtPlayerWithSetSpeed(hatcher, speed);
+
+        if (rotating)
+            transform.RotateAround(player.transform.position, rotationVector, 100*Time.deltaTime);
+
     }
-    
 
     IEnumerator Timer() {
-        yield return new WaitForSeconds(1.2f);
+        yield return new WaitForSeconds(0.75f);
         jump = false;
+        yield return new WaitForSeconds(1.5f);
+        rotating = false;
     }
 
 
@@ -52,9 +59,13 @@ public class HatcherMovement : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Player") {
             playerHP.TakeDamage();
+            rotating = true;
             jump = true;  
             StandardFireFunctions.FireDegreeOffsetFromPlayer(hatcher, 180);
             StartCoroutine(Timer());
+        }
+        if (collision.gameObject.layer == 10) { // Layer 10 == Ground
+            rotationVector = new Vector3(0,0,-rotationVector.z);
         } 
     }
 
