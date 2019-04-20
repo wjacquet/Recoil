bbd2f489abb9aaa6ecf3740fc43f0684d67eb4be
Walter Jacquette <wjacquet@purdue.edu>
2019-04-20 15:24:04 -0400

    Floater Functions

--- Recoil/Assets/Scripts/Enemies/Moon/FloaterFire.cs
+++ Recoil/Assets/Scripts/Enemies/Moon/FloaterFire.cs
@@ -6,25 +6,46 @@ public class FloaterFire : MonoBehaviour
 {
 
     public GameObject gravity_proj;
+    public GameObject photon_proj;
+
+    private Vector3[] firingPositions = new Vector3[4];
+    private int horizontalOffset = 9;
+    private int verticalOffsetSideShots = 8;
 
     void Start() 
     {
+        firingPositions[0] = new Vector3(transform.position.x + horizontalOffset, transform.position.y + verticalOffsetSideShots, transform.position.z);
+        firingPositions[1] = new Vector3(transform.position.x - horizontalOffset, transform.position.y + verticalOffsetSideShots, transform.position.z);
+        firingPositions[2] = new Vector3(transform.position.x, transform.position.y + 17, transform.position.z);
+        firingPositions[3] = new Vector3(transform.position.x, transform.position.y - 12, transform.position.z);
         StartCoroutine(FloaterPattern());
     }
 
     IEnumerator FloaterPattern() 
     {
         while (true) {
-            Shoot();
-            yield return new WaitForSeconds(1.0f);
+            ShootBasicProjectiles();
+            yield return new WaitForSeconds(1.5f);
+            ShootPhoton();
+            yield return new WaitForSeconds(0.5f);
         }
     }
 
-    void Shoot() 
+    void ShootBasicProjectiles() 
     {
-        GameObject proj = Instantiate(gravity_proj, transform.position, transform.rotation);
-        StandardFireFunctions.FireLeft(proj, 70);
-        proj = Instantiate(gravity_proj, transform.position, transform.rotation);
-        StandardFireFunctions.FireRight(proj, 70);
+        GameObject proj = Instantiate(gravity_proj, firingPositions[0], transform.rotation);
+        StandardFireFunctions.FireRight(proj, 60);
+        proj = Instantiate(gravity_proj, firingPositions[1], transform.rotation);
+        StandardFireFunctions.FireLeft(proj, 60);
+        proj = Instantiate(gravity_proj, firingPositions[2], transform.rotation);
+        StandardFireFunctions.FireVeticallyDegreeOffset(proj, 10);
+        proj = Instantiate(gravity_proj, firingPositions[2], transform.rotation);
+        StandardFireFunctions.FireVeticallyDegreeOffset(proj, -10);
+    }
+
+    void ShootPhoton() 
+    {
+        GameObject proj = Instantiate(photon_proj, firingPositions[3], transform.rotation);
+        StandardFireFunctions.FireDownFakeGravity(proj, 30);
     }
 }
