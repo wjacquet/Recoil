206424810d2c6189d825a67417037ba2b1435568
Nicholas Fordyce <FordyceNick@gmail.com>
2019-04-23 12:50:27 -0400

    Program Electric Trap

---
+++ Recoil/Assets/Scripts/Enemies/ElectricTrap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricTrap : MonoBehaviour
+{
+    public bool activated = true;
+    public GameObject beam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Toggle());
+    }
+
+    IEnumerator Toggle() {
+        while (true) {
+            yield return new WaitForSeconds(2.0f);
+            activated = !activated;
+            beam.SetActive(activated);
+        }
+    }
+}
