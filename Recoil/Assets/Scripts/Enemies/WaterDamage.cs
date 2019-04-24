0a27d405d73fd5c6f5035e797c418d127b1995dc
Nicholas Fordyce <FordyceNick@gmail.com>
2019-04-23 20:08:02 -0400

    Bubble provides immunity from water damage

--- Recoil/Assets/Scripts/Enemies/WaterDamage.cs
+++ Recoil/Assets/Scripts/Enemies/WaterDamage.cs
@@ -7,17 +7,19 @@ public class WaterDamage : MonoBehaviour
     GameObject player;
     PlayerHealth playerHP;
     bool damagePlayer = true;
+    private AbilitySelection abilitySelection;
     // Start is called before the first frame update
     void Start()
     {
         GameObject player = GameObject.Find("obj_player");
         playerHP = player.GetComponent<PlayerHealth>();
+        abilitySelection = GameObject.Find("Pause-Map-Screens").GetComponent<AbilitySelection>();
     }
 
     void OnTriggerStay2D(Collider2D collision) 
     {
         // Hurt over time
-        if (collision.gameObject.tag == "Player" && damagePlayer) {
+        if (collision.gameObject.tag == "Player" && AbilitySelection.currentAbility != "bubble" && damagePlayer) {
             playerHP.TakeDamage();
             damagePlayer = false;
             StartCoroutine(WaitForDamage());
