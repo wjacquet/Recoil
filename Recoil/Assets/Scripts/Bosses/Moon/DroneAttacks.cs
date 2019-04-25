e4f0cab738f9faa17c64c3a1bf67794ad33d2172
Walter Jacquette <wjacquet@purdue.edu>
2019-04-24 21:40:50 -0400

    Drone Moves and shoots

---
+++ Recoil/Assets/Scripts/Bosses/Moon/DroneAttacks.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneAttacks : MonoBehaviour
+{
+    public GameObject proj;
+    // Start is called before the first frame update
+
+    private Vector3 blasterPos;
+
+    GameObject tempProj;
+    void Start()
+    {
+        blasterPos = transform.position;
+        blasterPos.y = blasterPos.y - 18;
+        gameObject.GetComponent<ShipMovement>().TriggerRightMovement();
+        StartCoroutine(FireThreeDown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        blasterPos = transform.position;
+        blasterPos.y = blasterPos.y - 18;   
+    }
+
+
+    IEnumerator FireThreeDown() 
+    {
+        tempProj = Instantiate(proj, blasterPos, transform.rotation);
+        StandardFireFunctions.FireDownFakeGravity(tempProj, 100);
+        yield return new WaitForSeconds(0.75f);
+        tempProj = Instantiate(proj, blasterPos, transform.rotation);
+        StandardFireFunctions.FireDownFakeGravity(tempProj, 100);
+        yield return new WaitForSeconds(0.75f);
+        tempProj = Instantiate(proj, blasterPos, transform.rotation);
+        StandardFireFunctions.FireDownFakeGravity(tempProj, 100);
+        yield return new WaitForSeconds(0.75f);
+    }
+
+    IEnumerator FireWaves() 
+    {
+        tempProj = Instantiate(proj, blasterPos, transform.rotation);
+        StandardFireFunctions.FireDownFakeGravity(tempProj, 100);
+        yield return new WaitForSeconds(0.75f);
+        tempProj = Instantiate(proj, blasterPos, transform.rotation);
+        StandardFireFunctions.FireDownFakeGravity(tempProj, 100);
+        yield return new WaitForSeconds(0.75f);
+        tempProj = Instantiate(proj, blasterPos, transform.rotation);
+        StandardFireFunctions.FireDownFakeGravity(tempProj, 100);
+        yield return new WaitForSeconds(0.75f);
+    }
+}
---
+++ Recoil/Assets/Scripts/Bosses/Moon/ShipMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+
+    private bool moveLeft = false, moveRight = false, moveMid = false;
+    public Vector3 left, right, mid; 
+
+    private int baseMultiplier = 100;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (moveLeft) 
+        {
+            transform.position = Vector2.MoveTowards(transform.position, left, baseMultiplier * Time.deltaTime);
+        }
+        if (moveRight) 
+        {
+            transform.position = Vector2.MoveTowards(transform.position, right, baseMultiplier * Time.deltaTime);
+        }
+        if (moveMid) 
+        {
+            transform.position = Vector2.MoveTowards(transform.position, mid, baseMultiplier * Time.deltaTime);
+        }
+    }
+
+    public void TriggerLeftMovement() 
+    {
+        moveLeft = true;
+    }
+
+    public void TriggerRightMovement() 
+    {
+        moveRight = true;
+    }
+
+    public void TriggerMidMovement() 
+    {
+        moveMid = true;
+    }
+
+    public void ChangeMultiplier(int newMult) 
+    {
+        baseMultiplier = newMult;
+    }
+
+    public void StopMovement() 
+    {
+        moveLeft = false;
+        moveRight = false; 
+        moveMid = false;
+    }
+
+}
