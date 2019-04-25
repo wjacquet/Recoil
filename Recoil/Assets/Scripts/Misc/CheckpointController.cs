724b15c7aff766aad0d22728c6c03e76aa256633
Kyle Brown <brow1144@purdue.edu>
2019-04-25 13:47:37 -0400

    Add m into checkpoint

--- Recoil/Assets/Scripts/Misc/CheckpointController.cs
+++ Recoil/Assets/Scripts/Misc/CheckpointController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CheckpointController : MonoBehaviour {
 
     public GunSelection gunSelection;
+    public SpriteRenderer m;
 
     void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
             PlayerHealth playerHP = GameObject.Find("obj_player").GetComponent<PlayerHealth>();
             gunSelection.FlipCheckpoint(true);
+            m.enabled = true;
             playerHP.Heal();
             DataControl.Save();
         }
@@ -18,6 +20,7 @@ public class CheckpointController : MonoBehaviour {
     void OnTriggerExit2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
             gunSelection.FlipCheckpoint(false);
+            m.enabled = false;
             // playerHP.Heal();
             // DataControl.Save();
         }
