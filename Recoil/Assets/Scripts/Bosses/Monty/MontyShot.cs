449db67ec74edaaf4527eceeac1d64d2cff174c7
Riley Robertson <goodsmellinguy@gmail.com>
2019-04-25 01:31:41 -0400

    Monty

---
+++ Recoil/Assets/Scripts/Bosses/Monty/MontyShot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MontyShot : MonoBehaviour
+{
+    PlayerHealth playerHP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject player = GameObject.Find("obj_player");
+        playerHP = player.GetComponent<PlayerHealth>();
+        StartCoroutine(Timer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.Find("obj_player");
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, playerPos, 45 * Time.deltaTime);
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(5.0f);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerHP.TakeDamage();
+            Destroy(gameObject);
+        }
+
+    }
+}
