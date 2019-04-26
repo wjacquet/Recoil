627785d907a6f91b50c9c1f01370e40bae9d4407
Nicholas Fordyce <FordyceNick@gmail.com>
2019-04-26 11:24:19 -0400

    Add second attack to fish

---
+++ Recoil/Assets/Scripts/Bosses/Swarm/SwarmElectricity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmElectricity : MonoBehaviour
+{
+    PlayerHealth playerHP;
+    bool hurt = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject player = GameObject.Find("obj_player");
+        playerHP = player.GetComponent<PlayerHealth>();
+        StartCoroutine(Attack());
+    }
+
+    IEnumerator Attack() {
+        gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 1.0f);
+        yield return new WaitForSeconds(1.0f);
+        hurt = true;
+        gameObject.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        yield return new WaitForSeconds(1.0f);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerStay2D(Collider2D collision) {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player" && hurt) {
+            playerHP.TakeDamage();   
+        } 
+    }
+}
--- Recoil/Assets/Scripts/Bosses/Swarm/SwarmMovement.cs
+++ Recoil/Assets/Scripts/Bosses/Swarm/SwarmMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SwarmMovement : MonoBehaviour
 {
     public GameObject bullet;
+    public GameObject electricity;
     GameObject player;
     PlayerHealth playerHP;
     bool left = true;
@@ -55,10 +56,23 @@ public class SwarmMovement : MonoBehaviour
 
     IEnumerator SwarmFire() {
         while (true) {
-            GameObject projectile = Instantiate(bullet, transform.position, transform.rotation);
-            StandardFireFunctions.FireAtPlayer(projectile);
+            // homing shot
+            for (int i = 0;i < 6;i++) {
+                GameObject projectile = Instantiate(bullet, transform.position, transform.rotation);
+                StandardFireFunctions.FireAtPlayer(projectile);
 
-            yield return new WaitForSeconds(3.0f);
+                yield return new WaitForSeconds(3.0f);
+            }
+
+            yield return new WaitForSeconds(1.0f);
+
+            // electric fields
+            for (int i = 0;i < 4;i++) {
+                Vector3 electricPosition = new Vector3(player.transform.position.x, player.transform.position.y, -1);
+                Instantiate(electricity, electricPosition, transform.rotation);
+
+                yield return new WaitForSeconds(3.0f);
+            }
         }
     }
 
